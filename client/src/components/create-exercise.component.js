c975761c445a58b697c343daee18176477e751d6
areid987 <read.musik@gmail.com>
2020-01-26 16:40:40 -0600

    bind this for onChange to class in create-exercise component

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 export default class CreateExercise extends Component {
   constructor(props){
     super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+
     this.state = {
       username: '',
       description: '',
@@ -34,6 +40,21 @@ export default class CreateExercise extends Component {
     this.setState({
       date: e.target.value
     });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    }
+
+    console.log(exercise);
+
+    window.location = '/';
   }
 
   render() {
