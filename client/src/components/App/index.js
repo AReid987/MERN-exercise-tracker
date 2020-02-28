cff5bfe43949bddc37de61333fd47f1df9355b67
areid987 <read.musik@gmail.com>
2020-02-28 06:31:02 -0600

    added stub and routes for signup / login

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -6,6 +6,8 @@ import "./App.css";
 
 import Home from "../Home";
 import NotFound from "../NotFound";
+import Signup from "../Signup";
+import Login from "../Login";
 import Navbar from "../../components/navbar.component";
 import ExerciseList from "../../components/exercise-list.component";
 import EditExercise from "../../components/edit-exercise.component";
@@ -16,13 +18,17 @@ import CreateUser from "../../containers/user-form.container";
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <div className="container">
+      <Navbar />
+      </div>
       <div className="App">
         <header className="App-header">
           <Switch>
-            <Route exact path="/" component={() => <Home />} />
+            <Route exact path="/" component={Home} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
             <Route component={NotFound} />
-            <Navbar />
-            <br/>
+
             <Route path="/" exact component={ExerciseList} />
             <Route path="/edit/:id" component={EditExercise} />
             <Route path="/create" component={CreateExercise} />
